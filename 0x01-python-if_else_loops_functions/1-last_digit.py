f2cfc48202835ea6da1ceb78cf6a23fe58fb3ea1
Sarah-okolo <okolosarah402@gmail.com>
2022-11-29 19:44:20 +0100

    corrected number sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import random
+import math
 
 number = random.randint(-10000, 10000)
 numstr = repr(number)
 lastNum = numstr[-1:]
 lastNum = int(lastNum)
+
+if number < 0:
+    lastNum = -lastNum
+
 print(f"Last digit of {number:d} is {lastNum:d}", end=" ")
 
 if lastNum > 5:
