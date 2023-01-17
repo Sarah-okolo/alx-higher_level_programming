b15fde73889a0369264124dabd3ff9a83c8e77b5
Sarah-okolo <okolosarah402@gmail.com>
2023-01-17 15:23:52 +0100

    corrected the 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,15 @@
 
 import sys
 
-n = len(sys.argv) - 1
+n = len(sys.argv)
 
 if n < 1:
     print("{} arguments.".format(n))
 
 elif n == 1:
-    print("{} argument:\n{}: {}".format(n, n, sys.argv[1]))
+    print("{} argument:\n{}: {}".format(n, n, sys.argv[0]))
 
 else:
     print("{} arguments:".format(n))
-    for i in range(1, n+1):
-        print("{}: {}".format(i, sys.argv[i]))
-
-    print()
+    for i in range(0, n):
+        print("{}: {}".format(i+1, sys.argv[i]))
