6ffebde4f3efd40f9d7991168205e56225da907f
Sarah-okolo <okolosarah402@gmail.com>
2023-01-25 15:00:00 +0100

    created file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    elements_printed = 0
+
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            elements_printed += 1
+        except IndexError:
+            break
+    print()
+    
+    return(elements_printed)
