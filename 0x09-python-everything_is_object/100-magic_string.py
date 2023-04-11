630412c05bd03530779dd07285ae956b419f4548
Sarah-okolo <okolosarah402@gmail.com>
2023-04-11 06:55:44 -0700

    created the 100-magic_string.py file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-  magic_string.num = getattr(magic_string, 'num', 0) + 1
-  return ', '.join(['BestSchool' for i in range(magic_string.num)])
+    magic_string.num = getattr(magic_string, 'num', 0) + 1
+    return ', '.join(['BestSchool' for i in range(magic_string.num)])
